f8c8b5308720093e5a4781f9ecccb0e66a65e5fa
Andrey Zhozhin <andrey.zhozhin@gmail.com>
2012-09-29 15:34:45 +0600

    added range check for date

--- src/main/java/ru/xrm/app/transformers/JobDatePropertyTransformer.java
+++ src/main/java/ru/xrm/app/transformers/JobDatePropertyTransformer.java
@@ -24,14 +24,24 @@ public class JobDatePropertyTransformer implements PropertyTransformer {
 
 	public Object transform(String from) {
 		// 28 сентября 2012
-		String fromstr=(String)from;
-		String[] parts=fromstr.split(" ");
 		Calendar result;
-		int day=Integer.valueOf(parts[0]).intValue();
-		int month=months.indexOf(parts[1]);
-		int year=Integer.valueOf(parts[2]).intValue();
 		result=GregorianCalendar.getInstance();
-		result.set(year, month, day, 0, 0, 0);
+
+		String[] parts=from.split(" ");
+		
+		if (parts.length==3 && months.indexOf(parts[1])!=-1){
+			int day=Integer.valueOf(parts[0]).intValue();
+			int month=months.indexOf(parts[1]);
+			int year=Integer.valueOf(parts[2]).intValue();
+			if (day<=0 || day>31 || year<1970 || year > 2099){
+				result.set(1970, 0, 1, 0, 0, 0);
+			}else{
+				result.set(year, month, day, 0, 0, 0);
+			}
+		}else{
+			result.set(1970, 0, 1, 0, 0, 0);
+		}
+		
 		return result.getTime();
 	}
 
