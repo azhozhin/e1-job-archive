dd30fb3c75173ce80b60fe79456bfbf6dbc1f662
Andrey Zhozhin <andrey.zhozhin@gmail.com>
2012-10-02 20:53:20 +0600

    added id property to Section class

--- src/main/java/ru/xrm/app/domain/Section.java
+++ src/main/java/ru/xrm/app/domain/Section.java
@@ -3,6 +3,8 @@ package ru.xrm.app.domain;
 public class Section extends DomainObject{
 
 	// All fields should be protected for DomainObject methods
+	protected Long id;
+	
 	protected String name;
 	protected String href;
 	
@@ -21,19 +23,30 @@ public class Section extends DomainObject{
 		this.href=href;
 	}
 	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public String getHref() {
 		return href;
 	}
+	
 	public void setHref(String href) {
 		this.href = href;
 	}
-	
+
 	@Override
 	public boolean equals(Object o){
 		return name.equals((String)o);
