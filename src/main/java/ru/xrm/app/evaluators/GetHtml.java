efcd9ece827b68f028cdff2d55cf086a43d2d27a
Andrey Zhozhin <andrey.zhozhin@gmail.com>
2012-10-02 17:06:27 +0600

    renamed JobBodyPropertyElementEvaluator to GetHtml

--- src/main/java/ru/xrm/app/evaluators/GetHtml.java
+++ src/main/java/ru/xrm/app/evaluators/GetHtml.java
@@ -1,6 +1,6 @@
 package ru.xrm.app.evaluators;
 
-public class JobBodyPropertyElementEvaluator implements ElementEvaluator {
+public class GetHtml implements ElementEvaluator {
 
 	public String evaluate(org.jsoup.nodes.Element element) {
 		return element.html();
