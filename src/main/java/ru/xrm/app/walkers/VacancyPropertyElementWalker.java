dbe14892475e3fa03106dbf148385f8d21623277
Andrey Zhozhin <andrey.zhozhin@gmail.com>
2012-10-02 17:21:45 +0600

    Renamed JobPropertyElementWalker to VacancyPropertyElementWalker

--- src/main/java/ru/xrm/app/walkers/VacancyPropertyElementWalker.java
+++ src/main/java/ru/xrm/app/walkers/VacancyPropertyElementWalker.java
@@ -3,7 +3,7 @@ package ru.xrm.app.walkers;
 import org.jsoup.nodes.Element;
 
 
-public class JobPropertyElementWalker implements ElementWalker {
+public class VacancyPropertyElementWalker implements ElementWalker {
 
 	public Element walk(Element element) {
 		return element.parent().parent().child(1);
