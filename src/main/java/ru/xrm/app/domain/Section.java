438d491071a3abcf69a61c4049aaecab43dbf6fb
Andrey Zhozhin <andrey.zhozhin@gmail.com>
2012-10-04 16:42:13 +0600

    added equals and hashCode to Vacancy and Section

--- src/main/java/ru/xrm/app/domain/Section.java
+++ src/main/java/ru/xrm/app/domain/Section.java
@@ -13,34 +13,29 @@ public class Section extends DomainObject {
 
 	// All fields should be protected for DomainObject methods
 	@Id
-	@Column
+	@Column(name="section_id")
 	protected Long id;
 
-	@Column
+	@Column(name="name")
 	protected String name;
 
-	@Column
+	@Column(name="href")
 	protected String href;
 
 	@OneToMany(mappedBy = "section")
-	protected Set<Vacancy> vacancies;
+	protected Set<Vacancy> vacancies = new HashSet<Vacancy>();
 
 	public Section() {
-		this.name = "";
-		this.href = "";
-		this.vacancies = new HashSet<Vacancy>();
+		this("","");
 	}
 
 	public Section(String name) {
-		this.name = name;
-		this.href = "";
-		this.vacancies = new HashSet<Vacancy>();
+		this(name, "");
 	}
 
 	public Section(String name, String href) {
 		this.name = name;
 		this.href = href;
-		this.vacancies = new HashSet<Vacancy>();
 	}
 
 	public Long getId() {
@@ -76,8 +71,28 @@ public class Section extends DomainObject {
 	}
 
 	@Override
-	public boolean equals(Object o) {
-		return name.equals((String) o);
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Section other = (Section) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
 	}
 
 }
--- src/main/java/ru/xrm/app/domain/Vacancy.java
+++ src/main/java/ru/xrm/app/domain/Vacancy.java
@@ -6,6 +6,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 
 import org.hibernate.annotations.Type;
@@ -15,10 +16,10 @@ public class Vacancy extends DomainObject {
 
 	// All fields should be protected for DomainObject methods
 	@Id
-	@Column
+	@Column(name="vacancy_id")
 	protected Long id;
 
-	@Column
+	@Column(name="salary")
 	protected Integer salary;
 
 	@Column(name = "job_title")
@@ -27,24 +28,24 @@ public class Vacancy extends DomainObject {
 	@Column(name = "duty_type")
 	protected String dutyType;
 
-	@Column
+	@Column(name="education")
 	protected String education;
 
-	@Column
+	@Column(name="experience")
 	protected String experience;
 
-	@Column
+	@Column(name="schedule")
 	protected String schedule;
 
 	@ManyToOne
 	@JoinColumn(name = "section_id")
 	protected Section section;
 
-	@Column
+	@Column(name="city")
 	protected String city;
 
 	// TODO: make employer as separate class
-	@Column
+	@Column(name="employer")
 	protected String employer;
 
 	@Column(name = "vacancy_date")
@@ -57,7 +58,8 @@ public class Vacancy extends DomainObject {
 	@Column(name = "presented_by")
 	protected String presentedBy;
 
-	@Column
+	@Column(name="body")
+	@Lob 
 	protected String body;
 
 	public Long getId() {
@@ -170,6 +172,31 @@ public class Vacancy extends DomainObject {
 
 	public void setBody(String body) {
 		this.body = body;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Vacancy other = (Vacancy) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 
 }
