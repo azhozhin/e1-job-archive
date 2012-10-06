2280d6171a438e0e98d986759345ab3c76605487
Andrey Zhozhin <andrey.zhozhin@gmail.com>
2012-10-06 18:53:29 +0600

    added hashCode and equals to DutyType

--- src/main/java/ru/xrm/app/domain/DutyType.java
+++ src/main/java/ru/xrm/app/domain/DutyType.java
@@ -41,6 +41,31 @@ public class DutyType {
 		this.name = name;
 	}
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DutyType other = (DutyType) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
 	public void setProperty(String property, Object value)
 			throws SecurityException, NoSuchFieldException,
 			IllegalArgumentException, IllegalAccessException {
