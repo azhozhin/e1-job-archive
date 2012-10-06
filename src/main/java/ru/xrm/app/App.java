504c32d0446b446be6ae03d7ca98fecbabf43e35
Andrey Zhozhin <andrey.zhozhin@gmail.com>
2012-10-06 17:01:24 +0600

    Moved Site parsing to separate thread

--- src/main/java/ru/xrm/app/App.java
+++ src/main/java/ru/xrm/app/App.java
@@ -1,8 +1,10 @@
 package ru.xrm.app;
 
-import java.util.ArrayList;
+import java.lang.Thread.State;
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadMXBean;
+import java.util.ConcurrentModificationException;
 import java.util.Date;
-import java.util.Deque;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -10,8 +12,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 import org.hibernate.Session;
@@ -19,13 +19,9 @@ import org.hibernate.Session;
 import ru.xrm.app.config.Config;
 import ru.xrm.app.domain.Section;
 import ru.xrm.app.domain.Vacancy;
-import ru.xrm.app.httpclient.CachingHttpFetcher;
-import ru.xrm.app.httpclient.UrlHelper;
 import ru.xrm.app.misc.HibernateUtil;
-import ru.xrm.app.misc.VacancyPage;
-import ru.xrm.app.parsers.VacancyListOnePageParser;
-import ru.xrm.app.parsers.VacancySectionParser;
-import ru.xrm.app.threads.OnePageWorker;
+import ru.xrm.app.misc.SectionSet;
+import ru.xrm.app.threads.WholeSiteWorker;
 
 public class App 
 {
@@ -36,129 +32,60 @@ public class App
 	{
 		System.out.println(java.lang.Runtime.getRuntime().maxMemory()); 
 
-		UrlHelper urlHelper= UrlHelper.getInstance();
-		CachingHttpFetcher cf;
-		Config config=new Config();
+		Config config=Config.getInstance();
 		try{
 			config.load("config.xml");
 		}catch(Exception e){
 			e.printStackTrace();
 		}
 
-		String homePage="http://e1.ru/business/job";
-
-		String basename=urlHelper.getBasename(homePage);
-
-		cf=CachingHttpFetcher.getInstance();
-
-		// home page
-		String content = cf.fetchWithRetry(homePage,ENCODING,1000);
-
-		VacancySectionParser vsp=new VacancySectionParser(config, content);
-		List<Section> sections=vsp.parse();
-
-		Date d1=new Date();
-		ExecutorService executorService=Executors.newFixedThreadPool(THREAD_NUMBER);
-
 		List<Future<List<Vacancy>>> allVacancyListParts=new LinkedList<Future<List<Vacancy>>>();
 
-		// for all sections
-		for (Section section:sections){
-			System.out.format("\n*** NEW SECTION: %s ***\n",section.getName());
-			String vacancyListCurrentPageUrl=section.getHref();
+		WholeSiteWorker wholeSiteWorker = new WholeSiteWorker(allVacancyListParts, ENCODING, THREAD_NUMBER);
 
-			vacancyListCurrentPageUrl=urlHelper.constructAbsoluteUrl(vacancyListCurrentPageUrl, basename);
+		Thread wholeSiteWorkerThread=new Thread(wholeSiteWorker);
 
-			// get first page of vacancies
-			content=cf.fetchWithRetry(vacancyListCurrentPageUrl,ENCODING,1000);
-			VacancyListOnePageParser onePageParser=new VacancyListOnePageParser(config, content);
-			// get pages except current, link to first page we'll get from other pages
-			List<VacancyPage> pages=onePageParser.getPages();		
-			// if we have just one page, then getPages returns zero size list
-			if (pages.size()==0){
-				System.err.println("there is ONE page per Section");
-				pages.add(new VacancyPage(vacancyListCurrentPageUrl));
-			}
-
-			List<VacancyPage> allSectionPages=new ArrayList<VacancyPage>(pages);
-
-			Deque<VacancyPage> pageQueue = new LinkedList<VacancyPage>();
-			// push all pages to pageQueue
-			for (VacancyPage p:pages){
-				pageQueue.addLast(p);
-			}
-
-			int pageCounter=0;
-
-			// loop through all pages
-			while(true){
-				if (pageQueue.isEmpty())break;
-				VacancyPage p=pageQueue.poll();
-				String vacancyNextPageUrl=p.getHref();
-				vacancyNextPageUrl=urlHelper.constructAbsoluteUrl(vacancyNextPageUrl, basename);
-
-				System.out.format("%d ",pageCounter);
-				// try to add some new pages
-				content=cf.fetchWithRetry(vacancyNextPageUrl, ENCODING, 1000);
-				if (content==null){
-					System.err.format("Cannot download page %s adding it to end of queue\n",vacancyNextPageUrl);
-					pageQueue.addLast(p);
-					continue;
-				}
-
-				onePageParser.setHtml(content);
-				List<VacancyPage> newPages=onePageParser.getPages();
-				for (VacancyPage newPage:newPages){
-					boolean exists=false;
-					for (VacancyPage ap:allSectionPages){
-						if (ap.getHref().equals(newPage.getHref())){
-							exists=true;
-						}
-					}
-
-					if (!exists){
-						allSectionPages.add(newPage);
-						pageQueue.addLast(newPage);
-					}
-				}
-
-				// FIXME: should be vacancyNextPageUrl not vacancyListCurrentPageUrl
-				Future<List<Vacancy>> partVacancyList=executorService.submit(new OnePageWorker(config, vacancyNextPageUrl, basename, ENCODING, cf));
-				allVacancyListParts.add(partVacancyList);
-
-				pageCounter++;
-				if (pageCounter%30==0){
-					System.out.println();
-				}
-				//break; // FIXME
-			}// loop pages
-			System.out.println();
-			//break; // FIME
-		}// loop sections
-
-
-		System.out.format("<<< Done filling pages >>>\n");
+		wholeSiteWorkerThread.start();
 
 		// Wait for all threads ends
 		boolean allReady=false;
 
 		int threadCounter;
+		
+		ThreadMXBean threadBean = ManagementFactory.getThreadMXBean();
+	    
+
 
 		do{
 			threadCounter=0;
 			allReady=true;
-			for (Future<List<Vacancy>> f:allVacancyListParts){
-				if (!f.isDone()){
-					allReady=false;
-					threadCounter++;
+			
+			long[] threadIds = threadBean.findMonitorDeadlockedThreads();
+		    int deadlockedThreads = threadIds != null? threadIds.length : 0;
+		    
+			try{
+				for (Future<List<Vacancy>> f:allVacancyListParts){
+					if (!f.isDone()){
+						allReady=false;
+						threadCounter++;
+					}
 				}
+			}catch(ConcurrentModificationException e){
+				continue;
 			}
+
 			try {
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-			System.out.format("remaining tasks: %d \n", threadCounter);
+			
+		    
+			System.out.format("\n remaining tasks: %d deadlocked threads: %d\n", threadCounter,deadlockedThreads);
+			if (wholeSiteWorkerThread.getState()==State.RUNNABLE){
+				allReady=false;
+			}
+
 		}while(!allReady);
 
 		List<Vacancy> allVacancies=new LinkedList<Vacancy>();
@@ -203,31 +130,26 @@ public class App
 			System.out.format("%s \t %s\n",vacancy,stats.get(vacancy));
 		}
 
-		executorService.shutdown();
-		
-		Date d2=new Date();
-		System.out.format("\n %d ms. \n",d2.getTime()-d1.getTime());
-		System.out.println("Done Loading!");
-		
-		d1=new Date();
+		Date d1=new Date();
 		System.out.println("Start storing to database");
 		Session session=HibernateUtil.getSessionFactory().getCurrentSession();
 
 		// get stored section, delete them
 		session.beginTransaction();
-		
+
 		@SuppressWarnings("unchecked")
 		List<Section> allStoredSection = session.createQuery("from Section").list();
-		
+
 		@SuppressWarnings("unchecked")		
 		List<Vacancy> allStoredVacancies = session.createQuery("from Vacancy").list();
-		
+
 		allStoredSection.clear();
 		allStoredVacancies.clear();
 		session.getTransaction().commit();
 
 		session=HibernateUtil.getSessionFactory().getCurrentSession();
 		session.beginTransaction();
+		List<Section> sections=SectionSet.getInstance().getSections();
 		for (Section s:sections){
 			session.saveOrUpdate(s);
 		}
@@ -237,7 +159,7 @@ public class App
 		Set<Vacancy> vacs=new HashSet<Vacancy>();
 		for (Vacancy v:allVacancies){
 			c++;
-			
+
 			if (vacs.contains(v)){
 				System.out.format("Duplicate id %s\n",v.getId());
 				dups++;
@@ -248,7 +170,7 @@ public class App
 				session.saveOrUpdate(v);
 				vacs.add(v);
 			}
-			
+
 			if (c%20==0){
 				session.flush();
 				session.clear();
@@ -256,7 +178,7 @@ public class App
 		}
 		session.getTransaction().commit();
 		System.out.println("Done!");
-		d2=new Date();
+		Date d2=new Date();
 		System.out.format("\n %d ms. \n",d2.getTime()-d1.getTime());
 		System.out.format("Duplicates %d Updates %d\n", dups, upds);
 	}
