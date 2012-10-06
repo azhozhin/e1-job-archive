be6477114f072b7464ba7b2252f0153284072179
Andrey Zhozhin <andrey.zhozhin@gmail.com>
2012-10-07 01:45:42 +0600

    extended VacancyDAO interface with countByCategoryId method

--- src/main/java/ru/xrm/app/dao/VacancyDAO.java
+++ src/main/java/ru/xrm/app/dao/VacancyDAO.java
@@ -3,4 +3,5 @@ package ru.xrm.app.dao;
 import ru.xrm.app.domain.Vacancy;
 
 public interface VacancyDAO extends GenericDAO<Long, Vacancy>{
+	public Integer countByCategoryId(Long id);
 }
--- src/main/java/ru/xrm/app/dao/hibernate/impl/VacancyDAOHibernateImpl.java
+++ src/main/java/ru/xrm/app/dao/hibernate/impl/VacancyDAOHibernateImpl.java
@@ -1,7 +1,17 @@
 package ru.xrm.app.dao.hibernate.impl;
 
+import org.hibernate.Session;
+
 import ru.xrm.app.dao.VacancyDAO;
 import ru.xrm.app.domain.Vacancy;
+import ru.xrm.app.util.HibernateUtil;
 
 public class VacancyDAOHibernateImpl extends GenericDAOHibernateImpl<Long, Vacancy> implements VacancyDAO {
+
+	@Override
+	public Integer countByCategoryId(Long id) {
+		Session session=HibernateUtil.getSession();
+		Integer result = ((Long) session.getNamedQuery("Vacancy.countByCategoryId").setParameter("id", id).uniqueResult()).intValue();
+		return result;
+	}
 }
