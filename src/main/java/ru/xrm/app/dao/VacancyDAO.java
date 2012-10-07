b714ef001106d2f43da3e7ec4a900fa5d0236b27
Andrey Zhozhin <andrey.zhozhin@gmail.com>
2012-10-07 21:15:33 +0600

    added countByCategoryId and findByCategoryIdPagination to VacancyDAO

--- src/main/java/ru/xrm/app/dao/VacancyDAO.java
+++ src/main/java/ru/xrm/app/dao/VacancyDAO.java
@@ -1,7 +1,10 @@
 package ru.xrm.app.dao;
 
+import java.util.List;
+
 import ru.xrm.app.domain.Vacancy;
 
 public interface VacancyDAO extends GenericDAO<Long, Vacancy>{
-	public Integer countByCategoryId(Long id);
+	public Integer countByCategoryId(Long categoryId);
+	public List<Vacancy> findByCategoryIdPagination(Long categoryId, Integer startPosition, Integer perPage);
 }
--- src/main/java/ru/xrm/app/dao/hibernate/impl/VacancyDAOHibernateImpl.java
+++ src/main/java/ru/xrm/app/dao/hibernate/impl/VacancyDAOHibernateImpl.java
@@ -1,5 +1,8 @@
 package ru.xrm.app.dao.hibernate.impl;
 
+import java.util.List;
+
+import org.hibernate.Hibernate;
 import org.hibernate.Session;
 
 import ru.xrm.app.dao.VacancyDAO;
@@ -8,10 +11,24 @@ import ru.xrm.app.util.HibernateUtil;
 
 public class VacancyDAOHibernateImpl extends GenericDAOHibernateImpl<Long, Vacancy> implements VacancyDAO {
 
-	@Override
-	public Integer countByCategoryId(Long id) {
+	public Integer countByCategoryId(Long categoryId) {
 		Session session=HibernateUtil.getSession();
-		Integer result = ((Long) session.getNamedQuery("Vacancy.countByCategoryId").setParameter("id", id).uniqueResult()).intValue();
+		Integer result = ((Long) session.getNamedQuery("Vacancy.countByCategoryId")
+				.setParameter("id", categoryId)
+				.uniqueResult())
+				.intValue();
 		return result;
 	}
+
+	public List<Vacancy> findByCategoryIdPagination(Long categoryId, Integer startPosition, Integer perPage) {
+		Session session = HibernateUtil.getSession();
+		List<Vacancy> result = (List<Vacancy>)session.getNamedQuery("Vacancy.findByCategoryId")
+				.setParameter("id", categoryId)
+				.setFirstResult(startPosition)
+				.setMaxResults(perPage)
+				.list();
+		return result;
+	}
+
+	
 }
