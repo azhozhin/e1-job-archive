5a564203df47e161fb7119499defeed5fa18cb1d
Andrey Zhozhin <andrey.zhozhin@gmail.com>
2012-10-08 10:16:26 +0600

    extended VacancyDAO with countByCriterions and findManyPagination

--- src/main/java/ru/xrm/app/dao/VacancyDAO.java
+++ src/main/java/ru/xrm/app/dao/VacancyDAO.java
@@ -2,9 +2,13 @@ package ru.xrm.app.dao;
 
 import java.util.List;
 
+import org.hibernate.criterion.Criterion;
+
 import ru.xrm.app.domain.Vacancy;
 
 public interface VacancyDAO extends GenericDAO<Long, Vacancy>{
-	public Integer countByCategoryId(Long categoryId);
-	public List<Vacancy> findByCategoryIdPagination(Long categoryId, Integer startPosition, Integer perPage);
+	public Long countByCategoryId(Long categoryId);
+	public List<Vacancy> findByCategoryIdPagination(Long categoryId, Long startPosition, Long perPage);
+	public Long countByCriterions(Criterion... crits);
+	public List<Vacancy> findManyPagination(Long startPosition, Long perPage, Criterion... crits);
 }
--- src/main/java/ru/xrm/app/dao/hibernate/impl/VacancyDAOHibernateImpl.java
+++ src/main/java/ru/xrm/app/dao/hibernate/impl/VacancyDAOHibernateImpl.java
@@ -1,10 +1,12 @@
 package ru.xrm.app.dao.hibernate.impl;
 
+import java.util.LinkedList;
 import java.util.List;
 
-import org.hibernate.Hibernate;
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.Projections;
 
 import ru.xrm.app.dao.VacancyDAO;
 import ru.xrm.app.domain.Vacancy;
@@ -12,21 +14,21 @@ import ru.xrm.app.util.HibernateUtil;
 
 public class VacancyDAOHibernateImpl extends GenericDAOHibernateImpl<Long, Vacancy> implements VacancyDAO {
 
-	public Integer countByCategoryId(Long categoryId) {
+	public Long countByCategoryId(Long categoryId) {
 		Session session=HibernateUtil.getSession();
-		Integer result = ((Long) session.getNamedQuery("Vacancy.countByCategoryId")
+		Long result = (Long) session.getNamedQuery("Vacancy.countByCategoryId")
 				.setParameter("id", categoryId)
-				.uniqueResult())
-				.intValue();
+				.uniqueResult();
 		return result;
 	}
 
-	public List<Vacancy> findByCategoryIdPagination(Long categoryId, Integer startPosition, Integer perPage) {
+	public List<Vacancy> findByCategoryIdPagination(Long categoryId, Long startPosition, Long perPage) {
 		Session session = HibernateUtil.getSession();
+		@SuppressWarnings("unchecked")
 		List<Vacancy> result = (List<Vacancy>)session.getNamedQuery("Vacancy.findByCategoryId")
 				.setParameter("id", categoryId)
-				.setFirstResult(startPosition)
-				.setMaxResults(perPage)
+				.setFirstResult(startPosition.intValue())
+				.setMaxResults(perPage.intValue())
 				.list();
 		return result;
 	}
@@ -41,6 +43,30 @@ public class VacancyDAOHibernateImpl extends GenericDAOHibernateImpl<Long, Vacan
 
 	public List<Vacancy> findMany(Criterion... crits) {
 		return this.findMany(Vacancy.class, crits);
+	}
+
+	public Long countByCriterions(Criterion... crits) {
+		Session session=HibernateUtil.getSession();
+		Criteria criteria=session.createCriteria(Vacancy.class);
+		for (Criterion c:crits){
+			criteria.add(c);
+		}
+		criteria.setProjection(Projections.rowCount());
+		Long result = (Long)criteria.uniqueResult();
+		return result;
+	}
+
+	public List<Vacancy> findManyPagination(Long startPosition, Long perPage, Criterion... crits) {
+		Session session=HibernateUtil.getSession();
+		Criteria criteria=session.createCriteria(Vacancy.class);
+		for (Criterion c:crits){
+			criteria.add(c);
+		}
+		criteria.setFirstResult(startPosition.intValue());
+		criteria.setMaxResults(perPage.intValue());
+		@SuppressWarnings("unchecked")
+		List<Vacancy> result=(List<Vacancy>)criteria.list();
+		return result;
 	}
 
 	
